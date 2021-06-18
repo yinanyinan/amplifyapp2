a6e1630b30aed7c8c6c3cd0ccf6026a5d0b96f91
yinanyinan <yinan-huang@vorn.co.jp>
2021-06-18 09:58:29 +0900

    AppSync+Cognito OK

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,32 +1,19 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getTodo = /* GraphQL */ `
-  query GetTodo($id: ID!) {
-    getTodo(id: $id) {
-      id
-      name
-      description
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const listTodos = /* GraphQL */ `
-  query ListTodos(
-    $filter: ModelTodoFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        name
-        description
-        createdAt
-        updatedAt
+export const getdata = /* GraphQL */ `
+  query Getdata {
+    getdata {
+      statusCode
+      headers {
+        ContentType
       }
-      nextToken
+      body {
+        PK
+        ID
+        Pass
+        message
+      }
     }
   }
 `;
